868c9c7d415a01d37b65ed1a0d3535b264bbf1f8
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-12-04 10:52:26 +0600

    fixed some errors

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../Providers/AuthProviders";
+// import { useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { AuthContext } from "../Providers/AuthProviders";
 
 
 const axiosSecure = axios.create({
@@ -9,8 +9,8 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
-    const navigate = useNavigate()
-    const {logOut} = useContext(AuthContext)
+    // const navigate = useNavigate()
+    // const {logOut} = useContext(AuthContext)
 
     // request interceptor to add authorization header for every secure call to the api
     axiosSecure.interceptors.request.use(function(config){
@@ -24,17 +24,17 @@ const useAxiosSecure = () => {
       })
 
     //   intercepts 402 and 403
-    axiosSecure.interceptors.response.use(function(response) {
-        return response;
-    }, async (error)=>{
-        const status = error.response.status;
-        console.log('status error', status);
-        if(status === 401 || status === 403){
-            await logOut();
-            navigate('/login');
-        }
-        return Promise.reject(error)
-    })
+    // axiosSecure.interceptors.response.use(function(response) {
+    //     return response;
+    // }, async (error)=>{
+    //     const status = error.response.status;
+    //     console.log('status error', status);
+    //     if(status === 401 || status === 403){
+    //         await logOut();
+    //         navigate('/login');
+    //     }
+    //     return Promise.reject(error)
+    // })
 
 
 
